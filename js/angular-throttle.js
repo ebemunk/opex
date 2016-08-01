43925d01170b4cfd7a9789b9139b1caaafab0eed
ebemunk <ebemunk@gmail.com>
2016-08-01 10:11:59 -0700

    lf

--- js/angular-throttle.js
+++ js/angular-throttle.js
@@ -3,13 +3,13 @@ angular.module('throttle', [])
   return function (delay, no_trailing, callback, debounce_mode) {
     var timeout_id,
     last_exec = 0;
-    
+
     if (typeof no_trailing !== 'boolean') {
       debounce_mode = callback;
       callback = no_trailing;
       no_trailing = undefined;
     }
-    
+
     var wrapper = function () {
       var that = this,
           elapsed = +new Date() - last_exec,
@@ -21,7 +21,7 @@ angular.module('throttle', [])
           clear = function () {
             timeout_id = undefined;
           };
- 
+
       if (debounce_mode && !timeout_id) { exec(); }
       if (timeout_id) { $timeout.cancel(timeout_id); }
       if (debounce_mode === undefined && elapsed > delay) {
@@ -30,7 +30,7 @@ angular.module('throttle', [])
         timeout_id = $timeout(debounce_mode ? clear : exec, debounce_mode === undefined ? delay - elapsed : delay);
       }
     };
-    
+
     return wrapper;
   };
 }]);
